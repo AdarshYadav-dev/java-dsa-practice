3c8578b884faa3aa08036c453726dfb8070845ac
Adarsh Yadav <adarsh98215@gmail.com>
2026-05-20 03:05:24 +0530

    Added Day 2 work

---
+++ Day-2/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+    public static void main(String args[]){
+        System.out.print("Hello world\n");
+        System.out.print("Hello World\n");
+        System.out.print("hello world\n");
+        System.out.print("Hellow World\n");
+    }
+}
