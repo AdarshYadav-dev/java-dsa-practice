3752d18f70b9ca964f7e114aca12fdb6ee5b9e1c
Adarsh Yadav <adarsh98215@gmail.com>
2026-06-02 01:39:07 +0530

    Day 4: Java Data Types

---
+++ Day-4/Data_Type.java
@@ -0,0 +1,24 @@
+public class Data_Type {
+    public static void main(String args[]) {
+        int a = 10;
+        float b = 20.5f;
+        char c = 'A';
+        String d = "Hello";
+        boolean e = true;
+        long f = 123456789L;
+        double g = 3.14159;
+        short h = 100;
+        byte i = 127;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("String: " + d);
+        System.out.println("Boolean: " + e);
+        System.out.println("Long: " + f);
+        System.out.println("Double: " + g);
+        System.out.println("Short: " + h);
+        System.out.println("Byte: " + i);
+
+    }
+}
